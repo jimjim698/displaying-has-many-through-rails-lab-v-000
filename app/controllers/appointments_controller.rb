361332cae43d84b52b60d1e8662c5557a2897d6e
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-08 23:03:41 +0000

    automatically backed up by learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,6 @@
 class AppointmentsController < ApplicationController
   
   def show 
-    @appointment = Appointment.find(params[:id])
+    #@appointment = Appointment.find(params[:id])
   end 
 end
