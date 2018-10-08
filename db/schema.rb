10ae822738671658527c6d849898ade2728c91dc
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-08 22:40:56 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008222841) do
+ActiveRecord::Schema.define(version: 20181008223949) do
 
 # Could not dump table "appointments" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
